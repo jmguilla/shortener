224b236dc08dbb5381fca203d6a18c3983e48f10
Jean-michel GUILLAUME (contractor) <jean-michel.guillaume@amadeus.com>
2014-03-05 16:15:23 +0100

    App configured to deploy at ROOT

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,6 +11,7 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+grails.app.context = "/"
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
@@ -86,10 +87,15 @@ grails.hibernate.cache.queries = false
 environments {
     development {
         grails.logging.jul.usebridge = true
+        if(!System.getenv("APP_LAN_URL")){
+          println "No LAN address set, you can do it by setting APP_LAN_URL environment variable"
+        }else{
+          grails.serverURL = System.getenv("APP_LAN_URL")
+        }
     }
     production {
         grails.logging.jul.usebridge = false
-        // TODO: grails.serverURL = "http://www.changeme.com"
+        grails.serverURL = "http://www.changeme.com"
     }
 }
 
