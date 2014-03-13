4714b2421f5357a43ed195ed2a30fc0e46b3348b
Jean-michel GUILLAUME (contractor) <jean-michel.guillaume@amadeus.com>
2014-03-13 08:57:41 +0100

    Mapping.create and mapping.retrieve no more mocked. Create requires target as JSON in request's body

--- grails-app/controllers/com/kott/shortener/MappingController.groovy
+++ grails-app/controllers/com/kott/shortener/MappingController.groovy
@@ -5,6 +5,9 @@ import grails.plugin.springsecurity.annotation.Secured
 
 
 
+
+
+
 class MappingController {
 
   def mappingService
@@ -31,25 +34,31 @@ class MappingController {
     'IS_AUTHENTICATED_ANONYMOUSLY'
   ])
   def retrieve() {
+    def result = [status: 200]
+    if(!params.shortId){
+      result.status = 400
+      result.alert = 'danger'
+      result.message = 'shortId parameter is missing'
+    }else{
+      result.mapping = mappingService.retrieveFromShortId(params.shortId)
+      if(!result.mapping){
+        result.status = 404
+        result.alert = 'danger'
+        result.message = message(code: 'rest.mapping.notfound', default: "No mapping with such shortId: ${params.shortId}", args: [params.shortId])
+      }
+    }
     withFormat{
       html{
+        if(result.mapping){
+          response.sendRedirect(result.mapping.target)
+          response.sendError(500)
+        }else{
+          response.sendError(result.status, result.message)
+        }
       }
       json{
-        if(!params.shortId){
-          response.status = 400
-          render([alert: 'danger', message: 'shortId parameter is missing'] as JSON)
-          return
-        }else{
-          Mapping mapping = mappingService.retrieveFromShortId(params.shortId)
-          if(!mapping){
-            response.status = 404
-            render([alert: 'danger', message: message(code: 'rest.mapping.notfound', default: "No mapping with such shortId: ${params.shortId}", args: [params.shortId])] as JSON)
-            return
-          }else{
-            render([(params.shortId): mapping] as JSON)
-            return
-          }
-        }
+        response.status = result.status
+        render(result as JSON)
       }
     }
   }
@@ -57,9 +66,13 @@ class MappingController {
   /**
    * PUT: Creates a new {@link Mapping} and returns a JSON result with the following fields:
    * <ul>
-   *   <li>alert: mathing a value of <a href="http://getbootstrap.com/components/#alerts">http://getbootstrap.com/components/#alerts</a><li>
+   *   <li>alert: matching a value of <a href="http://getbootstrap.com/components/#alerts">http://getbootstrap.com/components/#alerts</a><li>
    *   <li>message: to describe what happened</a><li>
    *   <li>result: only in case of alert == 'success', the url of the newly created {@link Mappin}</li>
+   * </ul>
+   * params:
+   * <ul>
+   *  <li>target: the target url in a JSON object in request's body</li>
    * </ul>
    * 
    * GET: Renders the create view
@@ -72,18 +85,26 @@ class MappingController {
     if('get'.equalsIgnoreCase(request.method)){
       render view: 'create'
     }else if('put'.equalsIgnoreCase(request.method)){
-      Mapping result = new Mapping(target: "http://www.google.com")
-      if(!result.save(flush: true)){
+      if(!request.JSON.target){
+        response.status = 400
         render([
           alert: 'danger',
-          message: message(code: 'rest.mapping.create.failure', default: 'Mapping created', args: [result.errors as String])
+          message: message(code: 'rest.mapping.create.targetmissing', default: 'Target parameter required')
         ] as JSON)
       }else{
-        render([
-          alert: 'success',
-          message: message(code: 'rest.mapping.create.success', default: 'Mapping created'),
-          result: g.createLink(absolute: true, uri: '/') + mappingService.getShortId(result)
-        ] as JSON)
+        Mapping result = new Mapping(target: request.JSON.target)
+        if(!result.save(flush: true)){
+          render([
+            alert: 'danger',
+            message: message(code: 'rest.mapping.create.failure', default: 'Mapping created', args: [result.errors as String])
+          ] as JSON)
+        }else{
+          render([
+            alert: 'success',
+            message: message(code: 'rest.mapping.create.success', default: 'Mapping created'),
+            result: g.createLink(absolute: true, uri: '/') + mappingService.getShortId(result)
+          ] as JSON)
+        }
       }
     }
   }
