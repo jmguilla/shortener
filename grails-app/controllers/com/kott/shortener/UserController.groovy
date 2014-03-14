ad50b267da872cd0832db935d9e6a5268e2419a4
Jean-michel GUILLAUME (contractor) <jean-michel.guillaume@amadeus.com>
2014-03-14 18:22:30 +0100

    update and updatePWD

--- grails-app/controllers/com/kott/shortener/UserController.groovy
+++ grails-app/controllers/com/kott/shortener/UserController.groovy
@@ -6,28 +6,29 @@ import grails.transaction.Transactional
 
 import com.kott.shortener.user.exceptions.CannotCreateUserException
 
+
 class UserController {
 
-  static allowedMethods = [create: 'POST']
+  static allowedMethods = [create: 'POST', update: 'POST', updatePWD: 'POST']
 
   def emailConfirmationService
   def userService
   def springSecurityService
 
   def getUser() {
-	  def result = [:]
-	  def user = null
-	  if(springSecurityService.isLoggedIn()){
-		user = springSecurityService.getCurrentUser()
-	  }
-	  else{
-		  user = "not logged";
-	  }
-	  
-	  result.user = user
-	  render(result as JSON)
-	}
-  
+    def result = [:]
+    def user = null
+    if(springSecurityService.isLoggedIn()){
+      user = springSecurityService.getCurrentUser()
+    }
+    else{
+      user = "not logged";
+    }
+
+    result.user = user
+    render(result as JSON)
+  }
+
   @Transactional
   @Secured(['permitAll'])
   def create(){
@@ -61,20 +62,53 @@ class UserController {
     }else{
     }
   }
-  
+
   @Transactional
   @Secured(['IS_AUTHENTICATED_FULLY'])
   def update(){
-    withFormat{
-      html{
-        //renders the gsp
-      }
-      json{
-        render([
-          alert: 'success',
-          message: 'update performed (almost) successfully (reve pas hein... c\'pas fait)'
-        ])
-      }
+    try{
+      User me = springSecurityService.getCurrentUser()
+      bindData(me, request.JSON)
+      me.save(failOnError: true, flush: true)
+      render ( [alert: 'success', message: message(code: 'user.update.success', default: 'Update performed successfully')] as JSON)
+    }catch(Throwable t){
+      //TODO log here!
+      response.status = 400
+      render ( [alert: 'danger', message: message(code: 'user.update.failure', args: [t.toString()], default: 'Cannot performe the update: {0}')] as JSON)
+    }
+  }
+
+  @Secured(['IS_AUTHENTICATED_FULLY'])
+  def updatePWD(){
+    def current = request.JSON.current
+    def newPWD = request.JSON.newPWD
+    def newPWDAgain = request.JSON.newPWDAgain
+    if(!current || !newPWD || !newPWDAgain){
+      response.status = 400
+      render([alert: 'danger', message: message(code: 'user.pwd.update.paramsrequired')] as JSON)
+      return
+    }
+    if(!newPWD.equals(newPWDAgain)){
+      response.status = 400
+      render([alert: 'danger', message: message(code: 'user.pwd.update.wrongconfirmation')] as JSON)
+      return
+    }
+    def user = springSecurityService.getCurrentUser()
+    def userDetails = springSecurityService.userDetailsService.loadUserByUsername(user.username)
+    def salt = saltSource.getSalt(userDetails)
+    if(!springSecurityService.passwordEncoder.isPasswordValid(userDetails.password, current, salt)){
+      response.status = 400
+      render([alert: 'danger', message: message(code: 'user.pwd.update.missmatch')] as JSON)
+      return
+    }
+    user.password = newPWD
+    try{
+      user.save(failOnError: true)
+      render([alert: 'success', message: message(code: 'user.pwd.update.success')] as JSON)
+    }catch(Throwable t){
+      //TODO log here!
+      response.status = 400
+      render ( [alert: 'danger', message: message(code: 'user.update.failure', args: [t.toString()], default: 'Cannot perform the update: {0}')] as JSON)
     }
   }
 }
