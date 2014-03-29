b0dfaf015d205607ebdfaa1678e6256c5f3e0646
jean-michel guillaume <guillauj@gmail.com>
2014-03-29 14:53:28 +0100

    validation works for Mapping.target. It should be a valid URL

--- grails-app/domain/com/kott/shortener/Mapping.groovy
+++ grails-app/domain/com/kott/shortener/Mapping.groovy
@@ -1,36 +1,43 @@
 package com.kott.shortener
 
 class Mapping {
-  
+
   def mappingService
 
   def long id
 
   def String target
-  
-  
+
+
   def String shortId
-  
+
   static transients = ["shortId"]
   static belongsTo = [user: User]
 
   static constraints = {
     user nullable: true
-    target blank: false, nullable: false, size: 1..20000
+    target blank: false, nullable: false, size: 1..20000, validator: {val, obj, errors ->
+      try{
+        new URL(val)
+        return true
+      }catch(Throwable t){
+        errors.rejectValue('target', 'mapping.target.url.error', 'Target must be a valid URL')
+      }
+    }
   }
-  
+
   def afterInsert(){
     computeShortId()
   }
-  
+
   def afterUpdate(){
     computeShortId()
   }
-  
+
   def onLoad(){
     computeShortId()
   }
-  
+
   def computeShortId(){
     this.shortId = mappingService.getShortId(this)
   }
